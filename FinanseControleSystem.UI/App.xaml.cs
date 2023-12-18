72b0a4c55974922840f0199e40d656a38240734e
M_Karbainov <el_makso@mail.ru>
2023-12-18 21:08:22 +0300

    Initial project

---
+++ FinanseControleSystem.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FinanseControleSystem.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
